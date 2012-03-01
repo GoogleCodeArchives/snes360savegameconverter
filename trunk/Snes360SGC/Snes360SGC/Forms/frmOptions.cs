f225e1963034a88fdaf6f362e746af5635a01719
scuba156@gmail.com <scuba156@gmail.com@044a8167-ae4e-cc6d-bf3d-1ff89464e845>
2012-03-01 03:34:56 +0000

    +VersionInfo Class:Added +frmOptions Form:Added +frmUpdate Form:Added +frmAbout Form:Added +Main Form:Add frmAbout show form code +Main Form:Add exit code +Main Form:Add Tab View +Main Form:Add Menu Strip +Main Form:Add version string to window title

--- trunk/Snes360SGC/Snes360SGC/Forms/frmOptions.cs
+++ trunk/Snes360SGC/Snes360SGC/Forms/frmOptions.cs
@@ -7,11 +7,11 @@ using System.Linq;
 using System.Text;
 using System.Windows.Forms;
 
-namespace Snes360SGC
+namespace Snes360SGC.Forms
 {
-    public partial class Form1 : Form
+    public partial class frmOptions : Form
     {
-        public Form1()
+        public frmOptions()
         {
             InitializeComponent();
         }
---
+++ trunk/Snes360SGC/Snes360SGC/Tools/Version/VersionInfo.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Snes360SGC.Tools.Version
+{
+    class VersionInfo
+    {
+        #region Variables
+
+        #region struct
+
+        private struct versionInfo
+        {
+            # region Variables
+
+            private int Major { get; set; }
+            private int Minor { get; set; }
+            private int Build { get; set; }
+            private int Revision { get; set; }
+
+            #endregion
+
+        }
+
+        #endregion
+
+        #endregion
+    }
+}
